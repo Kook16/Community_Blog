84d7146b89a7a21d6b2d1d9b86dee90195423b8c
GODSPE1 <godspy18@gmail.com>
2024-05-22 16:45:17 +0100

    add some routes

--- app/routes.py
+++ app/routes.py
@@ -37,3 +37,22 @@ def home():
     }
     ]
     return render_template('home.html', title='Home', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/Contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
