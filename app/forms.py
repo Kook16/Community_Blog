55d0fe3eb20c73a07e2c711d363867d4cdae3970
Kok <jamesss075@gmail.com>
2024-05-28 10:38:34 +0300

    used bootstrap to style pages

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, TextAreaField, FileField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+from flask_wtf.file import FileAllowed
 
 
 class RegistrationForm(FlaskForm):
@@ -35,3 +36,10 @@ class ResetPassword(FlaskForm):
 class ResetConfirmationLink(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Resend')
+
+
+class UpdateProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    bio = TextAreaField('Bio', validators=[Length(max=500)])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
+    submit = SubmitField('Update')
--- app/models.py
+++ app/models.py
@@ -13,6 +13,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(128))
+    bio = db.Column(db.String(500))
     img = db.Column(db.String(128), default='default.png')
     is_active = db.Column(db.Boolean, default=False)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
